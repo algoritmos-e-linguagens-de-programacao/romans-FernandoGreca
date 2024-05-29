a578e6de0a4691ef7839ef98ddc8c5b5a0b6ab71
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-05-28 22:11:09 -0300

    Update romans.py
    Leve ajuste, mas exercício continua funcionando

--- 01/romans.py
+++ 01/romans.py
@@ -1,49 +1,52 @@
 def int_to_roman(num):
     sub = num
+    numero_romano = "" 
 
     while sub != 0:
         if sub == 4:
-            print("IV", end="")
+            numero_romano += "IV"
             sub -= 4
         elif sub == 9:
-            print("IX", end="")
+            numero_romano += "IX"
             sub -= 9
         elif sub >= 40 and sub < 50:
-            print("XL", end="")
+            numero_romano += "XL"
             sub -= 40
         elif sub >= 90 and sub < 100:
-            print("XC", end="")
+            numero_romano += "XC"
             sub -= 90
         elif sub >= 400 and sub < 500:
-            print("CD", end="")
+            numero_romano += "CD"
             sub -= 400
         elif sub >= 900 and sub < 1000:
-            print("CM", end="")
+            numero_romano += "CM"
             sub -= 900
         elif sub == 1:
-            print("I", end="")
+            numero_romano += "I"
             sub -= 1
         elif sub < 5:
-            print("I", end="")
+            numero_romano += "I"
             sub -= 1
         elif sub < 10:
-            print("V", end="")
+            numero_romano += "V"
             sub -= 5
         elif sub < 50:
-            print("X", end="")
+            numero_romano += "X"
             sub -= 10
         elif sub < 100:
-            print("L", end="")
+            numero_romano += "L"
             sub -= 50
         elif sub < 500:
-            print("C", end="")
+            numero_romano += "C"
             sub -= 100
         elif sub < 1000:
-            print("D", end="")
+            numero_romano += "D"
             sub -= 500
         elif sub >= 1000:
-            print("M", end="")
+            numero_romano += "M"
             sub -= 1000
+
+    return numero_romano
 
 def roman_to_int(s):
     def remover_caracteres(frase, caractere):
